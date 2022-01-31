145980d4a12ac58d73281c00ac849c66756d3062
Logan Saso <logansaso+tech@gmail.com>
2022-01-30 22:54:58 -0800

    Added modlog streaming support. Made stream resource impact lower

--- examples/stream-posts/main.go
+++ examples/stream-posts/main.go
@@ -8,7 +8,7 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/vartanbeno/go-reddit/v2/reddit"
+	"github.com/loganintech/go-reddit/v2/reddit"
 )
 
 var ctx = context.Background()
--- reddit/stream.go
+++ reddit/stream.go
@@ -11,6 +11,8 @@ type StreamService struct {
 	client *Client
 }
 
+var itemLimit = 100
+
 // Posts streams posts from the specified subreddit.
 // It returns 2 channels and a function:
 //   - a channel into which new posts will be sent
@@ -43,7 +45,8 @@ func (s *StreamService) Posts(subreddit string, opts ...StreamOpt) (<-chan *Post
 
 	// originally used the "before" parameter, but if that post gets deleted, subsequent requests
 	// would just return empty listings; easier to just keep track of all post ids encountered
-	ids := set{}
+	oldIDs := set{}
+	newIDs := set{}
 
 	go func() {
 		defer stop()
@@ -68,10 +71,16 @@ func (s *StreamService) Posts(subreddit string, opts ...StreamOpt) (<-chan *Post
 
 				// if this post id is already part of the set, it means that it and the ones
 				// after it in the list have already been streamed, so break out of the loop
-				if ids.Exists(id) {
+				if newIDs.Exists(id) || oldIDs.Exists(id) {
 					break
 				}
-				ids.Add(id)
+				newIDs.Add(id)
+
+				// If the new map is 10 times larger than item limit, make it the old map and clear it
+				if len(newIDs) >= itemLimit*10 {
+					oldIDs = newIDs
+					newIDs = make(map[string]struct{})
+				}
 
 				if streamConfig.DiscardInitial {
 					streamConfig.DiscardInitial = false
@@ -91,7 +100,7 @@ func (s *StreamService) Posts(subreddit string, opts ...StreamOpt) (<-chan *Post
 }
 
 func (s *StreamService) getPosts(subreddit string) ([]*Post, error) {
-	posts, _, err := s.client.Subreddit.NewPosts(context.Background(), subreddit, &ListOptions{Limit: 100})
+	posts, _, err := s.client.Subreddit.NewPosts(context.Background(), subreddit, &ListOptions{Limit: itemLimit})
 	return posts, err
 }
 
@@ -112,4 +121,89 @@ func (s set) Len() int {
 func (s set) Exists(v string) bool {
 	_, ok := s[v]
 	return ok
+}
+
+func (s *StreamService) getActions(subreddit string) ([]*ModAction, error) {
+	posts, _, err := s.client.Moderation.Actions(context.Background(), subreddit, &ListModActionOptions{ListOptions: ListOptions{Limit: itemLimit}})
+	return posts, err
+}
+
+// TODO: Generalize these two functions to have the same body... Maybe when generics is released ;)
+func (s *StreamService) Actions(subreddit string, opts ...StreamOpt) (<-chan *ModAction, <-chan error, func()) {
+	streamConfig := &streamConfig{
+		Interval:       defaultStreamInterval,
+		DiscardInitial: false,
+		MaxRequests:    0,
+	}
+	for _, opt := range opts {
+		opt(streamConfig)
+	}
+
+	ticker := time.NewTicker(streamConfig.Interval)
+	postsCh := make(chan *ModAction)
+	errsCh := make(chan error)
+
+	var once sync.Once
+	stop := func() {
+		once.Do(func() {
+			ticker.Stop()
+			close(postsCh)
+			close(errsCh)
+		})
+	}
+
+	// originally used the "before" parameter, but if that post gets deleted, subsequent requests
+	// would just return empty listings; easier to just keep track of all post ids encountered
+	oldIDs := set{}
+	newIDs := set{}
+
+	go func() {
+		defer stop()
+
+		var n int
+		infinite := streamConfig.MaxRequests == 0
+
+		for ; ; <-ticker.C {
+			n++
+
+			posts, err := s.getActions(subreddit)
+			if err != nil {
+				errsCh <- err
+				if !infinite && n >= streamConfig.MaxRequests {
+					break
+				}
+				continue
+			}
+
+			for _, post := range posts {
+				id := post.ID
+
+				// if this post id is already part of the set, it means that it and the ones
+				// after it in the list have already been streamed, so break out of the loop
+				if newIDs.Exists(id) || oldIDs.Exists(id) {
+					break
+				}
+				newIDs.Add(id)
+
+				// If the new map is 10 times larger than item limit, make it the old map and clear it
+				if len(newIDs) >= itemLimit*10 {
+					oldIDs = newIDs
+					newIDs = make(map[string]struct{})
+				}
+
+				if streamConfig.DiscardInitial {
+					streamConfig.DiscardInitial = false
+					break
+				}
+
+				postsCh <- post
+			}
+
+			if !infinite && n >= streamConfig.MaxRequests {
+				break
+			}
+		}
+	}()
+
+	return postsCh, errsCh, stop
 }
