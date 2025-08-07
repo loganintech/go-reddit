9acea7b9427af4f9a86a472c7dc69a319342f80e
Logan Saso <logansaso+tech@gmail.com>
2025-08-06 23:44:44 -0700

    This doesn't need to be a concrete type

--- reddit/streamer.go
+++ reddit/streamer.go
@@ -7,22 +7,22 @@ import (
 
 const defaultStreamInterval = time.Second * 5
 
-type streamConfig[T Streamable] struct {
+type streamConfig struct {
 	Interval       time.Duration
 	DiscardInitial bool
 	MaxRequests    int
 
 	StartFromFullID string
-	GetFunc         func(context.Context, string, string) ([]T, error)
+	GetFunc         func(context.Context, string, string) ([]Streamable, error)
 }
 
 // StreamOpt is a configuration option to configure a stream.
-type StreamOpt[T Streamable] func(*streamConfig[T])
+type StreamOpt func(*streamConfig)
 
 // WithStreamInterval sets the frequency at which data will be fetched for the stream.
 // If the duration is 0 or less, it will not be set and the default will be used.
-func WithStreamInterval[T Streamable](v time.Duration) StreamOpt[T] {
-	return func(c *streamConfig[T]) {
+func WithStreamInterval(v time.Duration) StreamOpt {
+	return func(c *streamConfig) {
 		if v > 0 {
 			c.Interval = v
 		}
@@ -30,28 +30,28 @@ func WithStreamInterval[T Streamable](v time.Duration) StreamOpt[T] {
 }
 
 // WithStreamDiscardInitial will discard data from the first fetch for the stream.
-func WithStreamDiscardInitial[T Streamable](c *streamConfig[T]) {
+func WithStreamDiscardInitial(c *streamConfig) {
 	c.DiscardInitial = true
 }
 
 // WithStreamMaxRequests sets a limit on the number of times data is fetched for a stream.
 // If less than or equal to 0, it is assumed to be infinite.
-func WithStreamMaxRequests[T Streamable](v int) StreamOpt[T] {
-	return func(c *streamConfig[T]) {
+func WithStreamMaxRequests(v int) StreamOpt {
+	return func(c *streamConfig) {
 		if v > 0 {
 			c.MaxRequests = v
 		}
 	}
 }
 
-func WithStartFromFullID[T Streamable](v string) StreamOpt[T] {
-	return func(c *streamConfig[T]) {
+func WithStartFromFullID(v string) StreamOpt {
+	return func(c *streamConfig) {
 		c.StartFromFullID = v
 	}
 }
 
-func WithGetFunc[T Streamable](f func(context.Context, string, string) ([]T, error)) StreamOpt[T] {
-	return func(c *streamConfig[T]) {
+func WithGetFunc(f func(context.Context, string, string) ([]Streamable, error)) StreamOpt {
+	return func(c *streamConfig) {
 		c.GetFunc = f
 	}
 }
